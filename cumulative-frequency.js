02f3c6d36e14fdbe6342a8b060bdc8ac606fb4fe
VishwanathAsundi <itzme.vishwanath.asundi@gmail.com>
2021-10-21 12:17:33 +0530

    Cumulative frequency of count of each element in an unsorted array

---
+++ cumulative-frequency.js
@@ -0,0 +1,17 @@
+function cumulativeFrequencyOfCount(a, n) {
+  let hm = new Array(n).fill(0);
+
+  for (let i = 0; i < n; i++) {
+    hm[a[i]]++;
+  }
+  console.log(hm, "hm");
+  for (let i = 0; i < n; i++) {
+    if (hm[a[i]] !== 0) {
+      document.write(a[i], "-->", hm[a[i]]);
+      document.write("<br/>");
+    }
+    hm[a[i]] = 0;
+  }
+}
+let a = [1, 3, 2, 4, 2, 1];
+cumulativeFrequencyOfCount(a, a.length);
