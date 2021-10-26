680d199167a15785c74027c19789ea2bfc4b9b55
VishwanathAsundi <itzme.vishwanath.asundi@gmail.com>
2021-10-26 12:11:33 +0530

    Find three element from different three arrays such that a + b + c = sum

---
+++ find-three-element-whose-sum-is-a+b+c=sum.js
@@ -0,0 +1,37 @@
+const findThree = function(a, b, c, sum) {
+  let map = {};
+  for (let i = 0; i < a.length; i++) {
+    if (!map[a[i]]) {
+      map[a[i]] = a[i];
+    }
+  }
+  let res = [];
+  for (let i = 0; i < b.length; i++) {
+    let current = sum - b[i];
+    for (let j = 0; j < c.length; j++) {
+      if (
+        map[current - c[j]] &&
+        JSON.stringify(res).indexOf(
+          JSON.stringify([current - c[j], b[i], c[j]])
+        ) == -1
+      ) {
+        res.push([current - c[j], b[i], c[j]]);
+        document.write(
+          "<br/> ",
+          "{",
+          current - c[j],
+          ", ",
+          b[i],
+          ", ",
+          c[j],
+          "}"
+        );
+      }
+    }
+  }
+};
+let a = [1, 2, 3, 4, 5],
+  b = [2, 3, 6, 1, 2],
+  c = [3, 2, 4, 5, 6];
+document.write("Trilets are :");
+findThree(a, b, c, 9);
